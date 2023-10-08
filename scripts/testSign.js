f525e1efbe1595f46ec07f34982225bb17d5dab2
0x2024antonio <0x2024antonio@gmail.com>
2023-10-08 15:22:07 -0400

    added testGenerateEthAddress.js and testSign.js

---
+++ scripts/testSign.js
@@ -0,0 +1,37 @@
+const {
+  getPrivateKey,
+  getPublicKey,
+  getEthAddress,
+} = require("./lib/generateEthAddress");
+
+const {
+  signMessage,
+  isSigned,
+  getRecoveredPublicKey,
+} = require("./lib/signMessage");
+
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes, hexToBytes } = require("ethereum-cryptography/utils");
+
+const { privateKey, privateKeyHex } = getPrivateKey();
+const { publicKey, publicKeyHex } = getPublicKey(hexToBytes(privateKeyHex));
+const ethAddress = getEthAddress(hexToBytes(publicKeyHex));
+
+console.log(privateKeyHex);
+console.log(publicKeyHex);
+console.log(ethAddress);
+
+const message = "Antonio-Ethereum";
+const messageHash = keccak256(utf8ToBytes(message));
+const signature = signMessage(messageHash, privateKey);
+const isSignValid = isSigned(signature, messageHash, publicKey);
+const recoveredPublicKeyPoint = signature.recoverPublicKey(messageHash);
+const { recoveredPublicKeyHex, recoveredPublicKey } = getRecoveredPublicKey(
+  recoveredPublicKeyPoint
+);
+const recoveredEthAddress = getEthAddress(recoveredPublicKey);
+
+console.log(isSignValid);
+console.log(recoveredPublicKeyPoint);
+console.log(recoveredPublicKeyHex);
+console.log(recoveredEthAddress);
