c969614c39282bfcc9d9f19a83ba8493c10354d1
0x2024antonio <0x2024antonio@gmail.com>
2023-10-08 22:34:06 -0400

    updated server to validate transaction by checking if sender equals to recovered eth address from messageHash and signature

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
+const { getEthAddress } = require("./lib/generateEthAddress");
+const {
+  getRecoveredPublicKeyFromSignatureObject,
+} = require("./lib/signMessage");
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -7,9 +12,12 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0x7C658Ec3540aFC8eFD6E74A2C03E9b8C8E93Dfdf": 100,
+  // privateKey: '3afc5d93aedd1567aa317bc04e0f318496678df9a3ff766dd1ad35449480dcfb'
+  "0x10EfAD1b1739645B6DF5DB559D51E6C1aCd3b239": 50,
+  // privateKey: '9b052baf28c2e3a077250f1a49682de7ea737c3839f6b8e5834f19c2ae496ee3'
+  "0x91a75B652a50eCfC2d58C70Cf0567E85839f4E74": 75,
+  // privateKey: '0e4d4cb5f6d5e5ec7674a9f15380bfa35d8d1a7ab178ce0070d4c0da13c6a522'
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,7 +27,31 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, recipient, amount, messageHashHex, signatureString } =
+    req.body;
+
+  /* Here we gets signature and messageHash from the request */
+  /* The goal is to recover an eth address from the above" */
+  /* If the recovered eth address is not the same as the sender, the transaction is not valid */
+
+  signatureObject = JSON.parse(signatureString, (key, value) => {
+    if (typeof value === "string" && value.startsWith("BIGINT::")) {
+      return BigInt(value.substring(8));
+    }
+    return value;
+  });
+
+  const { recoveredPublicKey } = getRecoveredPublicKeyFromSignatureObject(
+    signatureObject,
+    messageHashHex
+  );
+
+  const ethAddress = getEthAddress(recoveredPublicKey);
+
+  if (sender !== ethAddress) {
+    console.log("signature is not valid");
+    return false;
+  }
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
