c9ed1bb2aa332b8b54240ba8d7016f56ff489daa
0x2024antonio <0x2024antonio@gmail.com>
2023-10-08 22:33:16 -0400

    updated client to send messageHash and signature to server

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,7 +1,26 @@
 import { useState } from "react";
 import server from "./server";
+import { secp256k1 } from "ethereum-cryptography/secp256k1";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import { utf8ToBytes, hexToBytes, toHex } from "ethereum-cryptography/utils";
 
-function Transfer({ address, setBalance }) {
+const hashMessage = (message) => {
+  return keccak256(utf8ToBytes(message));
+};
+
+const signMessage = (messageHash, privateKeyBytes) => {
+  return secp256k1.sign(messageHash, privateKeyBytes);
+};
+
+const getSignatureString = (messageHash, privateKey) => {
+  const signature = signMessage(messageHash, hexToBytes(privateKey));
+  const signatureString = JSON.stringify(signature, (key, value) => {
+    return typeof value === "bigint" ? "BIGINT::" + value.toString() : value;
+  });
+  return signatureString;
+};
+
+function Transfer({ address, privateKey, setBalance }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
 
@@ -9,6 +28,9 @@ function Transfer({ address, setBalance }) {
 
   async function transfer(evt) {
     evt.preventDefault();
+    const message = `${address};${recipient};${sendAmount}`;
+    const messageHash = hashMessage(message);
+    const signatureString = getSignatureString(messageHash, privateKey);
 
     try {
       const {
@@ -16,6 +38,8 @@ function Transfer({ address, setBalance }) {
       } = await server.post(`send`, {
         sender: address,
         amount: parseInt(sendAmount),
+        messageHashHex: toHex(messageHash),
+        signatureString: signatureString,
         recipient,
       });
       setBalance(balance);
--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,6 +1,13 @@
 import server from "./server";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({
+  address,
+  setAddress,
+  balance,
+  setBalance,
+  privateKey,
+  setPrivateKey,
+}) {
   async function onChange(evt) {
     const address = evt.target.value;
     setAddress(address);
@@ -20,7 +27,22 @@ function Wallet({ address, setAddress, balance, setBalance }) {
 
       <label>
         Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        <input
+          placeholder="Type an address, for example: 0x1"
+          value={address}
+          onChange={onChange}
+        ></input>
+      </label>
+
+      <label>
+        Private Key
+        <input
+          placeholder="Input your private key for signing the transfer message"
+          value={privateKey}
+          onChange={(event) => {
+            setPrivateKey(event.target.value);
+          }}
+        ></input>
       </label>
 
       <div className="balance">Balance: {balance}</div>
